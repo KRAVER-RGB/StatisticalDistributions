49fa3d82e6f3ab8f2b7d6fb2a788b6db1b7c0777
LuisFernandoDF <luis.jimenez@definityfirst.com>
2025-03-25 23:31:48 -0600

    added jitpack configuration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,11 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
+
+group = "com.github.KRAVER-RGB"
+version = "1.0.0"
 
 kotlin {
     androidTarget {
@@ -31,6 +35,42 @@ kotlin {
         }
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["kotlin"])
+
+            groupId = "com.github.KRAVER-RGB"
+            artifactId = "StatisticalDistributions"
+            version = "1.0.0"
+
+            pom {
+                name.set("StatisticalDistributions")
+                description.set("Librería multiplataforma para distribuciones estadísticas")
+                url.set("https://github.com/KRAVER-RGB/StatisticalDistributions")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/KRAVER-RGB/StatisticalDistributions.git")
+                    developerConnection.set("scm:git:ssh://github.com:KRAVER-RGB/StatisticalDistributions.git")
+                    url.set("https://github.com/KRAVER-RGB/StatisticalDistributions")
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenLocal()
+    }
+}
+
+
 
 android {
     namespace = "org.luffer.statisticaldistributions.shared"
